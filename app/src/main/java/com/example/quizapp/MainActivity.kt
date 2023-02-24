8c48e2fd282bc4f38b1b58eeb5c3aa5898209541
joysarkar18 <joysarkar8171@gmail.com>
2023-02-24 19:13:21 +0530

    quiz app v1 incomplete

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+import com.example.quizapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        binding.EnterBtn.setOnClickListener {
+            if(binding.enteredName.text.toString().isEmpty()){
+                Toast.makeText(applicationContext , "Enter your name" , Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/QuestionAns.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionAns : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_ans)
+    }
+}
